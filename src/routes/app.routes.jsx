89d561d66d5316e355f4c9e59bfd0a914b59c592
Vitor <vitor.malmsteen@gmail.com>
2024-09-28 20:58:54 -0300

    asd

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,5 +1,4 @@
-import {Routes, Route} from 'react-router-dom'
-
+import {Routes, Route, Navigate} from 'react-router-dom'
 
 // app
 import {Home} from '../pages/Home'
@@ -7,16 +6,15 @@ import {CreateMovie} from '../pages/CreateMovie'
 import {MoviePreview} from '../pages/MoviePreview'
 import {User} from '../pages/User'
 
-
-
 export function AppRoutes() {
+    
     return (
     <Routes>
-        <Route path="/home" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/CreateMovie" element={<CreateMovie/>}/>
         <Route path="/MoviePreview" element={<MoviePreview/>}/>
         <Route path="/User" element={<User/>}/>
-        {/* <Route path="*" element={<Navigate to="/"/>} /> */}
+        <Route path="*" element={<Navigate to="/"/>} />
     </Routes>
     )
 }
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,21 +1,17 @@
-import {Routes, Route} from 'react-router-dom'   // Navigate
-
+import {Routes, Route, Navigate} from 'react-router-dom'   // Navigate
 
 // auth
 import {SignIn} from '../pages/SignIn'
 import {SignOut} from '../pages/SignOut'
 
-
-
 export function AuthRoutes() {
     const user = localStorage.getItem("@rocketnotes:user");
-    console.log(user)
     
     return (
     <Routes>
         <Route path="/" element={<SignIn/>}/>
         <Route path="/SignOut" element={<SignOut/>}/>
-        {/* { !user && <Route path="*" element={<Navigate to="/signin"/>} /> } */}
+        { !user && <Route path="*" element={<Navigate to="/"/>} /> }
     </Routes>
     )
 }
